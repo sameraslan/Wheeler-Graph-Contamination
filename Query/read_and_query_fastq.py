53d646eeb9481a1341f42b6eb7e68ef068204415
sameraslan <82460915+sameraslan@users.noreply.github.com>
2023-12-07 03:07:47 -0500

    Started implementing query for organism dot file

--- Query/read_and_query_fastq.py
+++ Query/read_and_query_fastq.py
@@ -2,7 +2,7 @@ import networkx as nx
 # import matplotlib.pyplot as plt
 
 DOTFILE = "../Kuanhao_WG_Toolkit/data/graph/DeBruijnGraph/DeBruijn_k_2_a1.dot"
-PATTERN = "GATCCA" # S1 -> S3 -> S6 -> S4 -> S8 -> S7
+PATTERN = "GATCCAAA" # S1 -> S3 -> S6 -> S4 -> S8 -> S7
 
 """
 Given a Wheeler Graph G and a pattern P, return the organism IDs that match the pattern.
@@ -41,11 +41,14 @@ def query_graph(G, pattern):
     path_graphs = []
 
     # for edge in dfs_edges[possible_starting_edge:]:
-    while starting_edge + current_pattern_idx <= len(dfs_edges):
+    while starting_edge + current_pattern_idx < len(dfs_edges):
         edge = dfs_edges[starting_edge + current_pattern_idx]
+        char, ids = parse_label(G.get_edge_data(edge[0], edge[1])[0]["label"])
+        # print(char, ids)
 
         # If new branch, reset from the cache
         if last_explored_node and last_explored_node != edge[0]:
+            print(pattern_idx_cache)
             current_pattern_idx = pattern_idx_cache[edge[0]]
             current_path_graph = path_graph_cache[edge[0]]
             # This is a hack, might collide with next_starting_edge?
@@ -58,13 +61,13 @@ def query_graph(G, pattern):
             path_graph_cache[edge[0]] = current_path_graph
 
         # If a non-starting edge equals the first char of the pattern, make a note of it
-        if G.get_edge_data(edge[0], edge[1])[0]["label"] == pattern[0]:
+        if char == pattern[0]:
             if next_starting_edge == starting_edge: # Ignore the rest if found
                 next_starting_edge = starting_edge + current_pattern_idx
         
         # ACTUAL MATCHING LOGIC STARTS
         # If pattern matches, append the organism id and increment the pattern index
-        if G.get_edge_data(edge[0], edge[1])[0]["label"] == pattern[current_pattern_idx]:
+        if char == pattern[current_pattern_idx]:
             nx.add_path(current_path_graph, [edge[0], edge[1]])
             current_pattern_idx += 1
 
@@ -92,8 +95,14 @@ def query_graph(G, pattern):
         last_explored_node = edge[1]
 
     # Not found
-    print("Not found")
+    print("Pattern not found")
     return []
+
+def parse_label(label):
+    label = label.strip('"')
+    char, ids = label.split(' ')
+    ids = ids.strip("()").strip('[]]').split(',')
+    return char, [int(id) for id in ids]
 
 if __name__ == "__main__":
     G = nx.nx_pydot.read_dot(DOTFILE)
@@ -101,7 +110,8 @@ if __name__ == "__main__":
     G.remove_edges_from([('S7', 'S6'), ('S2', 'S4')])
     # print(G.edges.data())
     a = query_graph(G, PATTERN)
-    print("Found {} match".format(len(a)))
+    
     if(len(a) > 0):
+        print("Found {} match".format(len(a)))
         print(a[0].edges)
         # [('S0', 'S1'), ('S1', 'S3'), ('S3', 'S6'), ('S6', 'S4'), ('S4', 'S8'), ('S8', 'S7')]
