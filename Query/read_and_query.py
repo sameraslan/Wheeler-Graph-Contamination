ceb2e6350a2adc2a33b9423b69dff7099851134d
sameraslan <82460915+sameraslan@users.noreply.github.com>
2023-12-07 19:41:47 -0500

    Small modification to query.py

--- Query/read_and_query.py
+++ Query/read_and_query.py
@@ -13,7 +13,6 @@ def extract_organism_ids(label):
 
 def query_graph(G, pattern):
     paths = []
-    organism_ids_sets = []
 
     # Look for starting nodes that correspond to the first char of the pattern
     for node in G.nodes:
@@ -36,18 +35,65 @@ def query_graph(G, pattern):
 
         paths = new_paths
         if not paths:
-            return []
+            return [], set()
 
-    print(paths)
-    # Collect all unique organism IDs from valid paths
-    valid_organism_ids = set()
-    for _, ids in paths:
-        valid_organism_ids.update(ids)
+    # If at least one valid path is found, return the organism IDs from any of the paths
+    if paths:
+        print(paths)
+        return paths[0][1]  # Only need to return the IDs from one path as all paths will have the same IDs
 
-    return valid_organism_ids
+    return set()
 
 if __name__ == "__main__":
     G = nx.nx_pydot.read_dot(DOTFILE)
-    pattern = "CTAG"
+    pattern = "CTAGG"
     organism_ids = query_graph(G, pattern)
-    print(organism_ids)
+    print(f"Organism(s) with this Pattern: {organism_ids}")
+
+# Ensure valid fatsq file
+def is_valid_input_file(input):
+    # Every valid FASTQ record should have exactly 4 lines
+    if len(input) % 4 != 0:
+        return False, "Error: The FASTQ file does not have a multiple of 4 lines."
+
+    for i in range(0, len(input), 4):
+        # The first line of each record should start with the "@" symbol
+        if not input[i].startswith("@"):
+            return False, f"Error: Line {i + 1} does not start with '@'."
+
+        # The third line of each record should start with the "+" symbol
+        if not input[i + 2].startswith("+"):
+            return False, f"Error: Line {i + 3} does not start with '+'."
+
+        # The sequence and quality strings should have the same length
+        if len(input[i + 1].strip()) != len(input[i + 3].strip()):
+            return False, f"Error: Length mismatch between sequence and quality on lines {i + 2} and {i + 4}."
+
+    return True, ""
+
+# Function to read input file
+def read_input_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    # Make sure we have a valid input file
+    is_valid, error_msg = is_valid_input_file(lines)
+    if not is_valid:
+        print(error_msg)
+        sys.exit(1)
+
+    reads = [line.strip() for line in lines[1::4]]  # Starting on line 1 every four lines
+    quality_chars = lines[3::4]  # Starting on line 3 every fout lines
+    quality_scores = [[ord(char) - 33 for char in line.strip()] for line in quality_chars]
+
+    return reads, quality_scores
+
+
+# if __name__ == "__main__":
+#     G = nx.nx_pydot.read_dot(DOTFILE)
+
+#     reads, quality_scores = read_input_file(FASTQ_FILE)
+
+#     for read in reads:
+#         paths, organism_ids = query_graph(G, read)
+#         print(organism_ids)
